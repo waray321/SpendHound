7138d91e97435fe1cfc2af5f3a1db06d64180b99
waray321 <fdc.waray@gmail.com>
2023-09-12 17:56:49 +0800

    Test002

---
+++ app/src/main/java/com/waray/spendhound/SpinnerItem.java
@@ -0,0 +1,42 @@
+package com.waray.spendhound;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class SpinnerItem extends ArrayAdapter<String> {
+
+    private final LayoutInflater mInflater;
+
+    public SpinnerItem(Context context, List<String> items) {
+        super(context, R.layout.spinner_item, items);
+        mInflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return createItemView(position, convertView, parent);
+    }
+
+    @Override
+    public View getDropDownView(int position, View convertView, ViewGroup parent) {
+        return createItemView(position, convertView, parent);
+    }
+
+    private View createItemView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.spinner_item, parent, false);
+        }
+
+        TextView textView = convertView.findViewById(android.R.id.text1);
+        textView.setText(getItem(position));
+
+        return convertView;
+    }
+}
+
