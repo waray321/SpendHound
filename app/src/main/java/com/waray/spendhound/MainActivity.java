d5ba0c4b8fde50e7597429b9e8bc5c36e680926a
waray <fdc.waray@gmail.com>
2023-09-14 01:38:35 +0800

    Commit

--- app/src/main/java/com/waray/spendhound/MainActivity.java
+++ app/src/main/java/com/waray/spendhound/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,29 +26,29 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.waray.spendhound.databinding.ActivityMainBinding;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Locale;
-import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView navView;
-    private TextView yesterday3TextView;
-    private TextView yesterday2TextView;
-    private TextView yesterday1TextView;
-    private TextView todayTextView;
-    private TextView tomorrow1TextView;
-    private TextView tomorrow2TextView;
-    private TextView tomorrow3TextView;
+    private TextView day7TextView;
+    private TextView day6TextView;
+    private TextView day5TextView;
+    private TextView day4TextView;
+    private TextView day3TextView;
+    private TextView day2TextView;
+    private TextView day1TextView;
     public FirebaseAuth mAuth;
     public int totalMonthSpends;
+    public int dailySpend;
+    public int i;
+    int desiredHeightInPixels;
+
 
 
     @Override
@@ -58,18 +59,20 @@ public class MainActivity extends AppCompatActivity {
 
         mAuth = DeclareDatabase.getAuth();
 
-        yesterday3TextView = findViewById(R.id.yesterday3);
-        yesterday2TextView = findViewById(R.id.yesterday2);
-        yesterday1TextView = findViewById(R.id.yesterday1);
-        todayTextView = findViewById(R.id.today);
-        tomorrow1TextView = findViewById(R.id.tomorrow1);
-        tomorrow2TextView = findViewById(R.id.tomorrow2);
-        tomorrow3TextView = findViewById(R.id.tomorrow3);
+        day7TextView = findViewById(R.id.day7);
+        day6TextView = findViewById(R.id.day6);
+        day5TextView = findViewById(R.id.day5);
+        day4TextView = findViewById(R.id.day4);
+        day3TextView = findViewById(R.id.day3);
+        day2TextView = findViewById(R.id.day2);
+        day1TextView = findViewById(R.id.day1);
 
         // Update the text of each TextView
         setTextViews();
 
         getTotalMonthSpends();
+
+        getEverydaySpends();
 
         navView = findViewById(R.id.navView);
         // Passing each menu ID as a set of Ids because each
@@ -127,22 +130,22 @@ public class MainActivity extends AppCompatActivity {
 
     public void setTextViews() {
         Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.DAY_OF_YEAR, -6); // Start with 3 days ago
+        calendar.add(Calendar.DAY_OF_YEAR, -6); // Start with 6 days ago
 
         // Set the text for each TextView
-        yesterday3TextView.setText(getFormattedDay(calendar));
+        day7TextView.setText(getFormattedDay(calendar));
         calendar.add(Calendar.DAY_OF_YEAR, 1);
-        yesterday2TextView.setText(getFormattedDay(calendar));
+        day6TextView.setText(getFormattedDay(calendar));
         calendar.add(Calendar.DAY_OF_YEAR, 1);
-        yesterday1TextView.setText(getFormattedDay(calendar));
+        day5TextView.setText(getFormattedDay(calendar));
         calendar.add(Calendar.DAY_OF_YEAR, 1);
-        todayTextView.setText(getFormattedDay(calendar));
+        day4TextView.setText(getFormattedDay(calendar));
         calendar.add(Calendar.DAY_OF_YEAR, 1);
-        tomorrow1TextView.setText(getFormattedDay(calendar));
+        day3TextView.setText(getFormattedDay(calendar));
         calendar.add(Calendar.DAY_OF_YEAR, 1);
-        tomorrow2TextView.setText(getFormattedDay(calendar));
+        day2TextView.setText(getFormattedDay(calendar));
         calendar.add(Calendar.DAY_OF_YEAR, 1);
-        tomorrow3TextView.setText(getFormattedDay(calendar));
+        day1TextView.setText(getFormattedDay(calendar));
     }
 
     private String getFormattedDay(Calendar calendar) {
@@ -197,5 +200,106 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void getEverydaySpends() {
+        // Create a reference to the "transactions" node
+        DatabaseReference databaseReference = DeclareDatabase.getDBRefTransaction();
+
+        // Get the current month in the format "MMMM-yyyy" (e.g., "September-2023")
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd", Locale.getDefault());
+
+        // Initialize an array to store daily spends
+        int[] last7DaysSpends = new int[7];
+
+        // Loop through the last 7 days
+        for (int i = 0; i < 7; i++) {
+            String currentDate = dateFormat.format(calendar.getTime());
+
+            // Create a child reference for the current date
+            DatabaseReference dateRef = databaseReference.child(currentDate);
+
+            // Capture the current value of i in a local variable
+            int dayNum = i;
+
+            // Add a listener to retrieve data for the current date
+            dateRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    for (DataSnapshot timeSnapshot : dataSnapshot.getChildren()) {
+                        Transaction transaction = timeSnapshot.getValue(Transaction.class);
+                        if (transaction != null) {
+                            // Retrieve the paymentAmount from the transaction
+                            int paymentAmount = transaction.getPaymentAmount();
+
+                            // Add the paymentAmount to dailySpend
+                            dailySpend += paymentAmount;
+                        }
+                    }
+                    last7DaysSpends[dayNum] = dailySpend;
+                    // Calculate the desired height based on the daily spend
+
+
+
+                    // Debug: Log daily spend
+                    Log.d("DailySpend", "Day " + dayNum + ": " + last7DaysSpends[dayNum]);
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    // Handle database read error
+                    String errorMessage = "Database read error occurred: " + databaseError.getMessage();
+                    Log.e("FirebaseDatabase", errorMessage);
+                }
+            });
+
+            // Move to the previous day
+            calendar.add(Calendar.DAY_OF_YEAR, -1);
+        }
+        // Set the height of the corresponding view
+        setViewHeightForDay(last7DaysSpends);
+    }
+
+    private void setViewHeightForDay(int[] last7DaysSpends) {
+        // Replace "your_view_id" with the actual ID of the corresponding view
+        int viewId = 0;
+
+        /*switch (day) {
+            case 0:
+                viewId = R.id.day1_bar;
+                break;
+            case 1:
+                viewId = R.id.day2_bar;
+                break;
+            case 2:
+                viewId = R.id.day3_bar;
+                break;
+            case 3:
+                viewId = R.id.day4_bar;
+                break;
+            case 4:
+                viewId = R.id.day5_bar;
+                break;
+            case 5:
+                viewId = R.id.day6_bar;
+                break;
+            case 6:
+                viewId = R.id.day7_bar;
+                break;
+        }
+
+        if (viewId != 0) {
+            View view = findViewById(viewId);
+            ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+            layoutParams.height = heightInPixels;
+            view.setLayoutParams(layoutParams);
+        }*/
+    }
+
+
+
+    // Utility method to show a toast message
+    private void showToast(String message) {
+        Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+    }
 
 }
