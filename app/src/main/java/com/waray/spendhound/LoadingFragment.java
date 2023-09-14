f0e779ca3e6edb6b19b6f473746acc5a896f1dec
waray321 <fdc.waray@gmail.com>
2023-09-14 17:57:40 +0800

    Office Dev

---
+++ app/src/main/java/com/waray/spendhound/LoadingFragment.java
@@ -0,0 +1,25 @@
+package com.waray.spendhound;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class LoadingFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_loading, container, false);
+    }
+
+    private FragmentManager getSupportFragmentManager() {
+        return null;
+    }
+
+}
+
